d5f434e820ea78c5958c67de38e8c016a6ea808d
chancayl <luisach94@gmail.com>
2019-11-18 13:37:57 +0000

    cors has been added

--- apps/app.js
+++ apps/app.js
@@ -3,6 +3,11 @@ const app = express();
 const apiRouter = require("../Routes/apiRouter");
 const { send405Error, handleCustError, handle400 } = require("./err/errors");
 
+import cors from "cors";
+import app from "express";
+
+app.use(cors());
+
 app.use(express.json());
 app.use("/api", apiRouter).all(send405Error)
 
