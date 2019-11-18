026d79a8bd2cf11965fc2772ce11af9a4c14c331
chancayl <luisach94@gmail.com>
2019-11-18 13:41:03 +0000

    cors has been updated

--- apps/app.js
+++ apps/app.js
@@ -3,13 +3,12 @@ const app = express();
 const apiRouter = require("../Routes/apiRouter");
 const { send405Error, handleCustError, handle400 } = require("./err/errors");
 
-import cors from "cors";
-import app from "express";
+const cors = require("cors");
 
 app.use(cors());
 
 app.use(express.json());
-app.use("/api", apiRouter).all(send405Error)
+app.use("/api", apiRouter).all(send405Error);
 
 app.use(handleCustError);
 app.use(handle400);
